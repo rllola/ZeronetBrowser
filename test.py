ee0a4eeed0b5a28877a89d1efe496a1ad5ef3b0e
Lola Dam <rllola80@gmail.com>
2018-11-03 03:06:55 +0100

    Windows build (#13)
    * Test installing pyqt5
    
    * Use 7z instead of unzip
    
    * added --retry option
    
    *  /download url
    
    * Use wget instead
    
    * timeout
    
    * remove unwanted wget
    
    * Try conda
    
    * I dont know what I am doing
    
    * We are under x64
    
    * Install from other repo on anaconda
    
    * install enum34
    
    * repo with python 2.7
    
    * say yes
    
    * test to only import pyqt5
    
    * run test
    
    * add -O option for download / try again building from source
    
    * try e instead of x option for extraction
    
    * Test
    
    * curl -LO git add appveyor.yml
    
    * keep full path for extraction 'x'
    
    * ok
    
    * added a dir
    
    * remove output extraction folder
    
    * lol
    
    * test
    
    * using nmake instead of make
    
    * nah
    
    * test test
    
    * Lol
    
    * ok
    
    * try choco
    
    * nieuce
    
    * Nieuce
    
    * op
    
    * Doh
    
    * ok
    
    * directory for cmake
    
    * look into cygwin
    
    * look into cygwin
    
    * using cygwin
    
    * using cygwin
    
    * using cygwin
    
    * not sure...
    
    * try
    
    * lal
    
    * lal
    
    * gcywin64
    
    * Look for path
    
    * Look for path
    
    * try to update var for nmake
    
    * add qt5.11.1 to PATH
    
    * Look for qmake
    
    * looking into bin folder
    
    * Added qmake path
    
    * kek
    
    * install enum34
    
    * disable qtNfc
    
    * disable qtNfc
    
    * import from PyQt5.QtWidgets
    
    * add Pyqt5 to pythonpath
    
    * look at sys.path in test
    
    * echo pythonpath
    
    * echo after set
    
    * ok
    
    * restart build ?
    
    * Try to get PyQt5.QtWidgets
    
    * Remove \sip\ path from PYTHONPATH
    
    * nah
    
    * Neh
    
    * Nieh
    
    * Change for qt 5.10.1
    
    * look into more dir
    
    * test test
    
    * Adding \libs to PYTHONPATH
    
    * k
    
    * test
    
    * Adding Windows_System path
    
    * Try with system32
    
    * Try again
    
    * Look for QT5 dll
    
    * Adding QT path to Python Path
    
    * Activate qt env variable script
    
    * After init
    
    * Copy all the dll to PyQt5 folder
    
    * Added Qt bin folder to PATH
    
    * Remove copy qt dlls for test with only path
    
    * Working ! Cleaning

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 import unittest
 import sys
-from PyQt5.QtWidgets import QApplication
+
+from PyQt5.QtCore import QUrl
+
+print sys.path
+print sys.modules
+
+from PyQt5 import QtWidgets
+
+# from PyQt5.QtWidgets import QApplication
 
 class TestBuild(unittest.TestCase):
 
     def test_pyqt5(self):
         # Need access to screen
-        #app = QApplication(sys.argv)
+        # app = QtWidgets.QApplication(sys.argv)
         pass
 
 if __name__ == '__main__':
