983c094fcf6a72c5b4cdfb7b3dcd8ef3b5c24a2d
Lola Dam <rllola80@gmail.com>
2018-11-06 14:26:32 +0100

    Fix 21 (#22)
    * close #19
    
    * fix #21

--- Browser.py
+++ Browser.py
@@ -1,10 +1,6 @@
 import PyQt5
 from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtWebKitWidgets import QWebView , QWebPage
-from PyQt5.QtWebEngineWidgets import *
-from PyQt5.QtWebKit import QWebSettings
-from PyQt5.QtNetwork import *
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 from optparse import OptionParser
 import os
 
