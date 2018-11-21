1dc988fc7a6d603c36331a51b153d8e28408c2f1
rllola <rllola80@gmail.com>
2018-11-21 11:19:52 +0100

    Install minimal package and not full

--- get-pyqt5-linux.sh
+++ get-pyqt5-linux.sh
@@ -16,7 +16,7 @@ wget -nv https://sourceforge.net/projects/pyqt/files/sip/sip-4.19.13/sip-4.19.13
 tar -xvzf sip-4.19.13.tar.gz
 cd sip-4.19.13
 python configure.py
-make -j
+make -j 8
 sudo make install
 echo "Done !"
 
@@ -27,6 +27,6 @@ wget -nv https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.10.1/PyQt5_gpl
 tar -xvzf PyQt5_gpl-5.10.1.tar.gz
 cd PyQt5_gpl-5.10.1
 python configure.py --confirm-license --disable=QtNfc --qmake=/opt/qt510/bin/qmake
-make -j
+make -j 8
 sudo make install
 echo "Done !"
