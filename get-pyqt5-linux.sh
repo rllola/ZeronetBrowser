cd2f735e6f89b060f253b6f2a9811df64a58764b
rllola <rllola80@gmail.com>
2018-11-27 15:56:35 +0100

    Main courante

--- get-pyqt5-linux.sh
+++ get-pyqt5-linux.sh
@@ -7,7 +7,7 @@ cd sip-4.19.13
 python configure.py --sip-module=PyQt5.sip --no-tools
 make -j 8
 sudo make install
-ls
+ls ./sipgen
 echo "Done !"
 
 cd ..
@@ -16,10 +16,8 @@ echo "=========== Install PyQt5 ==========="
 wget -nv https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.11.2/PyQt5_gpl-5.11.2.tar.gz
 tar -xvzf PyQt5_gpl-5.11.2.tar.gz
 cd PyQt5_gpl-5.11.2
-echo $PATH
-echo $PYTHONPATH
-ls /usr/lib/python2.7/dist-packages/PyQt5
-python configure.py --confirm-license --disable=QtNfc --qmake=/opt/qt511/bin/qmake --sip=/usr/lib/python2.7/dist-packages/PyQt5
+locate PyQt5.sip
+python configure.py --confirm-license --disable=QtNfc --qmake=/opt/qt511/bin/qmake
 make -j 8
 sudo make install
 echo "Done !"
