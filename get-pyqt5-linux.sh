f969b680f6df5fefbb4a1908d11bf8484cdfa175
rllola <rllola80@gmail.com>
2018-11-27 14:57:51 +0100

    Draaaaa

--- get-pyqt5-linux.sh
+++ get-pyqt5-linux.sh
@@ -4,7 +4,7 @@ echo "=========== Install SIP ==========="
 wget -nv https://sourceforge.net/projects/pyqt/files/sip/sip-4.19.13/sip-4.19.13.tar.gz
 tar -xvzf sip-4.19.13.tar.gz
 cd sip-4.19.13
-python configure.py --sip-module PyQt5.sip --no-tools
+python configure.py --sip-module=PyQt5.sip --no-tools
 make -j 8
 sudo make install
 echo "Done !"
@@ -15,7 +15,7 @@ echo "=========== Install PyQt5 ==========="
 wget -nv https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.11.2/PyQt5_gpl-5.11.2.tar.gz
 tar -xvzf PyQt5_gpl-5.11.2.tar.gz
 cd PyQt5_gpl-5.11.2
-python configure.py --confirm-license --disable=QtNfc --qmake=/opt/qt511/bin/qmake --sip=/usr/lib/python2.7/dist-packages/PyQt5
+python configure.py --confirm-license --disable=QtNfc --qmake=/opt/qt511/bin/qmake
 make -j 8
 sudo make install
 echo "Done !"
