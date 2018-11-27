fb2e3de1c7fbd7bc8bcd6fffbb48ee4bcd721c5b
rllola <rllola80@gmail.com>
2018-11-27 17:50:49 +0100

    Missing argument

--- get-pyqt5-linux.sh
+++ get-pyqt5-linux.sh
@@ -16,7 +16,7 @@ wget -nv https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.11.2/PyQt5_gpl
 tar -xvzf PyQt5_gpl-5.11.2.tar.gz
 cd PyQt5_gpl-5.11.2
 ls /usr/lib/python2.7/dist-packages
-python configure.py --confirm-license --disable=QtNfc --qmake=/opt/qt511/bin/qmake -n
+python configure.py --confirm-license --disable=QtNfc --qmake=/opt/qt511/bin/qmake -n PyQt5.sip
 make -j 8
 sudo make install
 echo "Done !"
