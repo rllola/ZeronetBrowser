be85d78e6bf3d76b0ed0806e113337a024b7c4c9
rllola <rllola80@gmail.com>
2018-11-27 11:47:16 +0100

    More print

--- launch.py
+++ launch.py
@@ -5,7 +5,7 @@ import os.path
 from MainWindow import MainWindow
 from ZeroNet import zeronet
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QLibraryInfo
+from PyQt5 import QtCore
 from multiprocessing import Process
 import time
 
@@ -21,8 +21,25 @@ if __name__ == '__main__':
 
     # Start the PyQt application
     app = QApplication(sys.argv)
-    print QLibraryInfo.location(QLibraryInfo.LibraryExecutablesPath)
-
+    print("Python sys.executable:\n\t%s" % sys.executable)
+    print("Python sys.path:\n\t%s" % "\n\t".join(sys.path))
+    print('Qt5 Libraries path:\n\t%s' % \
+            QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.LibrariesPath))
+    print('Qt5 Library Executables path:\n\t%s' % \
+            QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.LibraryExecutablesPath))
+    print('Qt5 Binaries path:\n\t%s' % \
+            QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.BinariesPath))
+    print('Qt5 Data path:\n\t%s' % \
+            QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.DataPath))
+    print('Qt5 Imports path:\n\t%s' % \
+            QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.ImportsPath))
+    print('Qt5 Plugins path:\n\t%s' % \
+            QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.PluginsPath))
+    print('Qt5 Settings path:\n\t%s' % \
+            QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.SettingsPath))
+    print('Qt5 Prefix path:\n\t%s' % \
+            QtCore.QLibraryInfo.location(QtCore.QLibraryInfo.PrefixPath))
+    print("Qt5 app.libraryPaths():\n\t%s" % "\n\t".join((app.libraryPaths())))
     mainWindow = MainWindow()
     app.exec_()
 
