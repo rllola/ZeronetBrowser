be0e4da5465f12e4d1c797c1e763fe05147bc84c
rllola <rllola80@gmail.com>
2018-11-29 16:52:15 +0100

    Clean branch fix-46

--- get-pyqt5-osx.sh
+++ get-pyqt5-osx.sh
@@ -7,7 +7,7 @@ cd sip-4.19.13
 python configure.py --sip-module=PyQt5.sip
 make -j 8
 sudo make install
-sudo touch /usr/lib/python2.7/dist-packages/PyQt5/__init__.py
+sudo touch /usr/local/Cellar/python@2/2.7.15_1/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/PyQt5/__init__.py
 echo "Done !"
 
 cd ..
@@ -16,7 +16,8 @@ echo "=========== Install PyQt5 ==========="
 wget -nv https://sourceforge.net/projects/pyqt/files/PyQt5/PyQt-5.11.2/PyQt5_gpl-5.11.2.tar.gz
 tar -xvzf PyQt5_gpl-5.11.2.tar.gz
 cd PyQt5_gpl-5.11.2
-python configure.py --confirm-license --disable=QtNfc --qmake=/opt/qt511/bin/qmake -n PyQt5.sip
+ls /usr/local/Cellar/python@2/2.7.15_1/Frameworks/Python.framework/Versions/2.7/bin/
+python configure.py --confirm-license --disable=QtNfc --qmake=/usr/local/opt/qt5/bin/qmake -n PyQt5.sip --sip=/usr/local/Cellar/python@2/2.7.15_1/Frameworks/Python.framework/Versions/2.7/bin/sip
 make -j 8
 sudo make install
 echo "Done !"
