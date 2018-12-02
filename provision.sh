1882330468b90481ce866661fe86e41bf94fbd49
rllola <rllola80@gmail.com>
2018-12-02 07:55:44 +0100

    fix #52

--- provision.sh
+++ provision.sh
@@ -10,13 +10,15 @@ sudo add-apt-repository --yes ppa:beineri/opt-qt-5.11.2-xenial
 sudo apt-get update
 
 # Install Qt5, QtWebEngine and QtSvg
-sudo apt-get install -y msgpack-python python-gevent python-dev build-essential libgl1-mesa-dev qt511-meta-minimal qt511webengine qt511svg mlocate
+sudo apt-get install -y msgpack-python python-gevent python-dev build-essential libgl1-mesa-dev qt511-meta-minimal qt511webengine qt511svg
 
-chmod +x ./get-pyqt5-linux.sh
-./get-pyqt5-linux.sh
+chmod +x ./Browser/get-pyqt5-linux.sh
+./Browser/get-pyqt5-linux.sh
 
 echo "===== INSTALL PYINSTALLER ====="
 #export LC_ALL="en_US.UTF-8"
 #export LC_CTYPE="en_US.UTF-8"
 #sudo dpkg-reconfigure locales
 sudo pip install pyinstaller
+# Avoid missing _vendor.sox error
+sudo pip install --upgrade setuptools
