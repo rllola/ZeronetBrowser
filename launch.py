6c1cd027bf7d626f181212dde9420e2b719c7d2d
rllola <rllola80@gmail.com>
2019-01-24 18:53:11 +0100

    Added print

--- launch.py
+++ launch.py
@@ -5,6 +5,7 @@ import os
 import ConfigParser
 from src.MainWindow import MainWindow
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QLibraryInfo
 from multiprocessing import Process, freeze_support
 from ZeroNet import zeronet
 import time
@@ -99,6 +100,23 @@ if __name__ == '__main__':
 
     # Start the PyQt application
     app = QApplication(sys.argv)
+
+    print "PrefixPath : " + QLibraryInfo.location(QLibraryInfo.PrefixPath)
+    print "DocumentationPath : " + QLibraryInfo.location(QLibraryInfo.DocumentationPath)
+    print "HeadersPath : " + QLibraryInfo.location(QLibraryInfo.HeadersPath)
+    print "LibrariesPath : " + QLibraryInfo.location(QLibraryInfo.LibrariesPath)
+    print "LibraryExecutablesPath : " + QLibraryInfo.location(QLibraryInfo.LibraryExecutablesPath)
+    print "BinariesPath : " + QLibraryInfo.location(QLibraryInfo.BinariesPath)
+    print "PluginsPath : " + QLibraryInfo.location(QLibraryInfo.PluginsPath)
+    print "ImportsPath : " + QLibraryInfo.location(QLibraryInfo.ImportsPath)
+    print "Qml2ImportsPath : " + QLibraryInfo.location(QLibraryInfo.Qml2ImportsPath)
+    print "ArchDataPath : " + QLibraryInfo.location(QLibraryInfo.ArchDataPath)
+    print "DataPath : " + QLibraryInfo.location(QLibraryInfo.DataPath)
+    print "TranslationsPath : " + QLibraryInfo.location(QLibraryInfo.TranslationsPath)
+    print "ExamplesPath : " + QLibraryInfo.location(QLibraryInfo.ExamplesPath)
+    print "TestsPath : " + QLibraryInfo.location(QLibraryInfo.TestsPath)
+    print "SettingsPath : " + QLibraryInfo.location(QLibraryInfo.SettingsPath)
+
     mainWindow = MainWindow(**kwargs)
     app.exec_()
 
