dc92bd77a3c69fec096d5f1cc0b2ecd7ff7a5b63
rllola <rllola80@gmail.com>
2019-03-25 18:13:37 +0100

    Remove useless line

--- get-pyqt5-linux.sh
+++ get-pyqt5-linux.sh
@@ -15,7 +15,7 @@ cd sip-$PYQTSIP_VERSION
 python configure.py --sip-module=PyQt5.sip
 make -j $NP
 sudo make install
-sudo touch /usr/lib/python2.7/dist-packages/PyQt5/__init__.py
+#sudo touch /usr/lib/python2.7/dist-packages/PyQt5/__init__.py
 echo "Done !"
 
 cd ..
@@ -24,7 +24,7 @@ echo "=========== Install PyQt5 ==========="
 wget -nv https://www.riverbankcomputing.com/static/Downloads/PyQt5/PyQt5_gpl-$QT5_VERSION.tar.gz
 tar -xvzf PyQt5_gpl-$QT5_VERSION.tar.gz
 cd PyQt5_gpl-$QT5_VERSION
-LD_LIBRARY_PATH=$QT5_PATH/lib python configure.py --confirm-license --no-docstrings --no-designer-plugin --no-tools --enable=QtWidgets --enable=QtCore --enable=QtGui --enable=QtPrintSupport --enable=QtPositioning --enable=QtNetwork --enable=QtQuick --enable=QtQuickWidgets --enable=QtWebChannel --enable=QtQml --qmake=$QT5_PATH/bin/qmake QMAKE_LFLAGS_RPATH=
+python configure.py --confirm-license --no-docstrings --no-designer-plugin --no-tools --enable=QtWidgets --enable=QtCore --enable=QtGui --enable=QtPrintSupport --enable=QtPositioning --enable=QtNetwork --enable=QtQuick --enable=QtQuickWidgets --enable=QtWebChannel --enable=QtQml --qmake=$QT5_PATH/bin/qmake
 make -j $NP
 sudo make install
 echo "Done !"
