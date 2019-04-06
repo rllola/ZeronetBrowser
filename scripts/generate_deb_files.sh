1a51ab91c9763d3856bc46743bd5cadae8fac083
rllola <rllola80@gmail.com>
2019-04-06 19:36:49 +0200

    Create proper version files from travis tag

---
+++ scripts/generate_deb_files.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "====== GENERATE CONTROL FILE ======"
+cat > $PWD/pkg-debian/DEBIAN/control << EOF
+Package: ZeronetBrowser
+Version: ${TRAVIS_TAG/v}
+Architecture: all
+Essential: no
+Section: web
+Priority: optional
+Maintainer: Lola Rigaut-Luczak
+Description: Browser for Zeronet.
+EOF
+
+echo "====== GENERATE .DESKTOP FILE ======"
+cat > $PWD/pkg-debian/usr/share/applications/zeronet-browser.desktop << EOF
+[Desktop Entry]
+Version=${TRAVIS_TAG/v}
+Name=ZeroNet Browser
+Comment=ZeroNet browser is a dedicated browser for ZeroNet protocol.
+Exec=ZeronetBrowser %u
+Path=/usr/share/ZeronetBrowser/
+Icon=/usr/share/ZeronetBrowser/icons/zeronet-logo.svg
+Terminal=true
+Type=Application
+Categories=Application;Network;
+MimeType=x-scheme-handler/zero;
+EOF
