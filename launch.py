6347951c7d48960f75b987f2d114a13769a0ed6d
Lola Dam <contact@loladam.com>
2019-07-09 16:16:01 +0200

    Actually fix a lot of shitty things (#104)
    * Get config and plugin
    
    * Ignore log folder
    
    * Remove data folder from ZeroNet folder (gitignore)
    
    * Need too create log folder in user folder
    
    * Clean code to remove data folder
    
    * Remove the data folder in the nsi installer script
    
    * Comment line which seems to break appveyor but not sure why
    
    * No test

--- launch.py
+++ launch.py
@@ -14,7 +14,7 @@ import time
 import imp
 
 def osx_first_run():
-    zeronet_browser_path = os.path.join(os.sep, os.path.expanduser("~"), "Library", "Application Support", "Zeronet Browser")
+    zeronet_browser_path = os.path.join(os.path.expanduser("~"), "Library", "Application Support", "Zeronet Browser")
 
     try:
         os.makedirs(zeronet_browser_path)
@@ -23,16 +23,16 @@ def osx_first_run():
             raise
 
     try:
-        os.makedirs(os.path.join(os.sep, zeronet_browser_path, "data"))
+        os.makedirs(os.path.join(zeronet_browser_path, "data"))
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
 
     # Create lock.pid file
-    open(os.path.join(os.sep, zeronet_browser_path, "data", "lock.pid"), "w").close()
+    open(os.path.join(zeronet_browser_path, "lock.pid"), "w").close()
 
     # Create zeronet.conf file
-    f = open(os.path.join(os.sep, zeronet_browser_path, "zeronet.conf"), 'w')
+    f = open(os.path.join(zeronet_browser_path, "zeronet.conf"), 'w')
     f.write("[global]\n")
     f.write("data_dir = {} \n".format(zeronet_browser_path))
     f.close()
@@ -58,6 +58,10 @@ def openLocked(path, mode="wb"):
 
 if __name__ == '__main__':
     freeze_support()
+
+    # Adding plugin repo (The plugins could placed somewhere else)
+    app_dir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, os.path.join(app_dir,"ZeroNet"))  # Imports relative to ZeroNet
 
     url = None
     if len(sys.argv) > 1 and sys.argv[1].startswith('zero:'):
@@ -94,22 +98,22 @@ if __name__ == '__main__':
     try:
         zeronet_path = config.get('global', 'data_dir')
     except configparser.Error:
-        zeronet_path = os.path.join(os.sep, os.getcwd(), "ZeroNet")
+        zeronet_path = os.path.join(os.getcwd(), "ZeroNet")
 
     if zeronet_path:
         # See if it is already running
         try:
-            lock = openLocked(os.path.join(os.sep, zeronet_path, "data", "lock.pid"), "w")
+            lock = openLocked(os.path.join(zeronet_path, "lock.pid"), "w")
             lock.close()
             # Create a process for Zeronet using this version of ZeroNet
-            p = Process(target=zeronet.main)
+            p = Process(target=zeronet.start)
             p.start()
         except BlockingIOError as err:
             print(err)
             print("Can't open lock file, your ZeroNet client is probably already running, opening browser without starting Zeronet in the background...")
     else:
         # Create a process for Zeronet
-        p = Process(target=zeronet.main)
+        p = Process(target=zeronet.start)
         p.start()
 
     time.sleep(5)
